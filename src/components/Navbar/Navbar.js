907a11093b698ef8b27ea810fa7bd71f0700dbf4
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-21 23:58:59 -0600

    Formatted code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,35 +1,36 @@
-import React, {useState} from 'react'
-import {Nav, Hamburger,Logo, Menu, MenuLink, MenuButton} from './NavbarElements';
-import GrayLogo from '../../Assets/Logos/gray-logo.png';
+import React, { useState } from "react";
+import {
+  Nav,
+  Hamburger,
+  Logo,
+  Menu,
+  MenuLink,
+  MenuButton,
+} from "./NavbarElements";
+import GrayLogo from "../../Assets/Logos/gray-logo.png";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-
-const [isOpen, setIsOpen] = useState(false);
-
-
-
-    return (
-      <Nav>
-<Logo src={GrayLogo}/>
-<Hamburger onClick={() => setIsOpen(!isOpen)}>
-    <span/>
-    <span/>
-    <span/>
-    <span/>
-</Hamburger>
-<Menu isOpen={isOpen}>
-<MenuLink href=''>News</MenuLink>
-<MenuLink href=''>Tools</MenuLink>
-<MenuLink href=''>About Us</MenuLink>
-<MenuLink href=''> Blog</MenuLink>
-<MenuLink href=''>Upcoming Events</MenuLink>
-<MenuButton>Login</MenuButton>
-<MenuButton>Join Our Server</MenuButton>
-</Menu>
-
-
-      </Nav>
-    )
+  return (
+    <Nav>
+      <Logo src={GrayLogo} />
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+        <MenuLink href="">News</MenuLink>
+        <MenuLink href="">Tools</MenuLink>
+        <MenuLink href="">About Us</MenuLink>
+        <MenuLink href=""> Blog</MenuLink>
+        <MenuLink href="">Upcoming Events</MenuLink>
+        <MenuButton>Login</MenuButton>
+        <MenuButton>Join Our Server</MenuButton>
+      </Menu>
+    </Nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
